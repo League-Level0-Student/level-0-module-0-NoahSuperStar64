478b331f021a73b5bf8126574d1e961e57c86462
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-29 13:03:47 -0700

    l0m0 knock knock

---
+++ src/strings_and_dialogs/MoringZombie.java
@@ -0,0 +1,13 @@
+package strings_and_dialogs;
+
+import javax.swing.JOptionPane;
+
+public class MoringZombie {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "Mission Objective: Remove self from item:bed");
+	JOptionPane.showMessageDialog(null, "Mission Objective: Prepare and consume item:nutrients");
+JOptionPane.showMessageDialog(null, "Mission Objective: Care for entity:PET_NAME");
+JOptionPane.showMessageDialog(null, "Mission Objective: Switch item:outfitOld with item:outfitNew");
+JOptionPane.showMessageDialog(null, "Mission Objective: Download DayPlan47.csv from HowToBeHuman.com");
+}
+}
